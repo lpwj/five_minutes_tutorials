bbeb39877585bd3f3d1dfb3285eb74feb3495266
Joel Carneiro <jolasman@hotmail.com>
2022-10-01 13:56:36 +0100

    adding sphinx doc tutorial code

---
+++ sphinx_documentation/src/main.py
@@ -0,0 +1,12 @@
+"""This is the main python module where we call all the function to do 
+something...
+"""
+
+
+def main():
+    """This is the main function we call when running the python file."""
+    pass
+
+
+if __name__ == "__main__":
+    main()
---
+++ sphinx_documentation/src/utils/utils1.py
@@ -0,0 +1,59 @@
+"""This is the doc string for the utils1 file where we can say things about the python module.add()
+We can write long text if we want.
+
+* topic 1
+* topic 2
+"""
+
+
+def utils1_func1():
+    """This function does nothing."""
+    pass
+
+
+def utils1_func2(param1: int, param2: int):
+    """This function returns the sum of the values you pass in as arguments.
+
+    Args:
+        param1 (int): Integer to sum.
+        param2 (int): Integer to sum.
+
+    Returns:
+        int: Sum of the given parameter's values.
+    """
+    return param1 + param2
+
+
+def utils1_func3():
+    """This function does nothing."""
+    pass
+
+
+def utils1_func4(param1: str, param2: int, param3: bool):
+    """"This function returns the 10 or the param2 times 3 if ...
+
+
+    Args:
+        param1 (str): String to check if we should do something.
+        param2 (int): Base value to the multiplication.
+        param3 (bool): If we should do the multiplication or not.
+
+    Returns:
+        int: _description_
+    """
+    if param3 and param1 == "Y":
+        return param2 * 3
+    return 10
+
+
+def utils1_func5(param1: str, param2: int):
+    """This function returns true.
+
+    Args:
+        param1 (str): Some String...
+        param2 (int): Some Integer...
+
+    Returns:
+        boolean: Always true.
+    """
+    return True
